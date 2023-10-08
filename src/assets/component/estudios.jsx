7c085a5242365831d01fecc5fcddce4b92b74401
Zunilda <zunilda.encina02@gmail.com>
2023-10-08 19:13:17 -0300

    Estilos

--- src/assets/component/estudios.jsx
+++ src/assets/component/estudios.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './estudios.css'
 function estudios() {
   return (
+    <div className='caja'>
     <div className='galeria'>
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-   <img src="https://drive.google.com/uc?id=118dfEJt5G3WXyNJrNJSmjMlL2C5hwaGV" alt="JavaScript" />
-      
+   <img className='estudio'  src="https://drive.google.com/uc?id=1FZ6a0Vnw5j-eyTnztCg3DgVh4NErt6sW" alt="Impresion 3D" />
+   <img className='estudio' src="https://drive.google.com/uc?id=1FPmRbEWvh3bSQgmNlP3JyzL4KPJqjLr7" alt="Desarrollo de juegos" />
+   <img className='estudio' src="https://drive.google.com/uc?id=1FJXGhwpOSRE0Mf89UibL7qad3g_9gn9v" alt="Frond-end 3" />
+   <img className='estudio' src="https://drive.google.com/uc?id=1Fb2XqitDvr_tIW5pXRfKsOXijjN1NqXp" alt="Frond-end 2" />
+   <img className='estudio' src="https://drive.google.com/uc?id=1FbERWQzDM_ln0kP1hkMRIl4GYHN3zQuf" alt="Frond-end 1" />
+   <img className='estudio' src="https://drive.google.com/uc?id=1EcoxZPa4le2H-06zPOppZ5pSZT3EyDpf" alt="Tech" />
+    </div>
     </div>
   )
 }
